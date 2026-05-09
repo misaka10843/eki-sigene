dd851fca02dad4eee54ec49c132cf7cd25e9c30d
misaka10843 <misaka10843@outlook.jp>
2026-05-10 00:57:08 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE ?? '/',
   plugins: [
     tailwindcss(),
     vue(),
